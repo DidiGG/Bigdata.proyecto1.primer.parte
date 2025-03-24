f427fdd84afd6e3fa153a9cd88522396b997aa2e
Didi <ddgrisalesg@uqvirtual.edu.co>
2025-03-23 23:07:24 -0500

    .

--- app/proyecto1-parte2.py
+++ app/proyecto1-parte2.py
@@ -3,6 +3,7 @@ import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
+# Cargar datos
 datos_videos = r'C:\Users\didie\OneDrive\Documents\GitHub\Bigdata.proyecto1.primer.parte\data\MXvideos.csv'
 
 df = pd.read_csv(datos_videos, encoding="latin-1")
@@ -11,19 +12,19 @@ print(df.head())
 
 
 # Seleccionar la columna de títulos
-titles = df['title'].dropna().tolist()  # Convertir a lista y eliminar valores nulos
+titles = df['title'].dropna().tolist()  
 
 # Aplicar TF-IDF
 vectorizer = TfidfVectorizer(stop_words='english')  # Se eliminan stopwords en inglés
 tfidf_matrix = vectorizer.fit_transform(titles)  # Convertir títulos en vectores TF-IDF
 
 # Elegir un título y calcular similitudes
-titulo_consulta = "El viaje de chihiro"  # Cambia esto por el título que desees
-query_vector = vectorizer.transform([titulo_consulta])  # Vectorizar el título de consulta
-similarities = cosine_similarity(query_vector, tfidf_matrix).flatten()  # Calcular similitudes
+titulo_consulta = "El viaje de chihiro" 
+query_vector = vectorizer.transform([titulo_consulta])  # 
+similarities = cosine_similarity(query_vector, tfidf_matrix).flatten()  
 
 # Obtener los 10 títulos más similares
-top_indices = similarities.argsort()[-11:-1][::-1]  # Obtener los 10 más altos (excluyendo el propio)
+top_indices = similarities.argsort()[-11:-1][::-1] 
 top_titles = [(titles[i], similarities[i]) for i in top_indices]
 
 # Imprimir resultados
